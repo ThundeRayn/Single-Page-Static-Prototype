7c44a453a56f5764cb77aaa629a976d8799302ba
Your Name <tangshirong477@gmail.com>
2025-07-13 15:14:59 -0400

    responsive nav in progres

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import './index.css';
 import LoadingScreen from './components/LoadingScreen';
 import Hero from './components/sections/Hero';
 import { useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   const [isLoaded,setIsLoaded] = useState(false);
+  const [menuOpen,setMenuOpen] = useState(false);
 
   return (
     <>
     {!isLoaded && <LoadingScreen onComplete={()=>setIsLoaded(true)}/>}
     <div className={`min-h-screen transition-opacity duration-700 ${isLoaded? "opacity-100" : "opacity-0"}`}>
+      <Navbar menuOpen={menuOpen} setMenuOpen={()=>setMenuOpen(true)}/>
       <Hero/>
     </div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,42 @@
+import { useEffect } from "react";
+import { FaBuildingCircleCheck } from "react-icons/fa6";
 
+interface myProps{
+    menuOpen:boolean;
+    setMenuOpen:()=>void;
+}
 
-const Navbar = () => {
+const Navbar:React.FC<myProps> = ({menuOpen,setMenuOpen}) => {
+    useEffect(()=>{
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    },[menuOpen])
+
   return (
-    <div>
-        
+    <div className="fixed top-0 w-full z-40 bg-stone-700 text-white backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <div className="flex justify-between">
+            <div className="max-w-5xl p-4">
+                <a href="#home">
+                    <FaBuildingCircleCheck style={{ display: 'inline-block'}}/>
+                    <span className="pl-4">Yonge Street</span>
+                </a>
+                
+            </div>
+
+            {menuOpen? <span>hamburger</span> : <ul className="flex gap-2 jusfity-center">
+                <li className=" hover:bg-stone-600 p-4">
+                    <a href="#details">details</a></li>
+                <li className=" hover:bg-stone-600 p-4">
+                    <a href="#contact">contact</a></li>
+            </ul>}
+            {/* <ul className="flex gap-2 jusfity-center">
+                <li className=" hover:bg-stone-600 p-4">
+                    <a href="#details">details</a></li>
+                <li className=" hover:bg-stone-600 p-4">
+                    <a href="#contact">contact</a></li>
+            </ul> */}
+        </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
